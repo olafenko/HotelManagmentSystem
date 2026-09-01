f40929b6805dc0f1adb3fa27fe6b4f2c73a0f6a4
olafenko <olopikolo666@gmail.com>
2026-09-01 19:20:34 +0200

    install jwt bearer package and create scratch of token service

---
+++ HotelManageSys.API/Features/Auth/Messages/Commands/LoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace HotelManageSys.API.Auth.DTO_s;
+
+public class LoginCommand : IRequest<bool>
+{
+    public required string Login { get; set; }
+    public required string Password { get; set; }
+
+}
---
+++ HotelManageSys.API/Features/Auth/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using HotelManageSys.API.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HotelManageSys.API.Auth;
+
+public class TokenService
+{
+
+    private readonly IConfiguration _config;
+    private readonly string _secretKey;
+    private readonly int _accessTokenExpirationMinutes;
+
+    public TokenService(IConfiguration config, string secretKey, int accessTokenExpirationMinutes)
+    {
+        _config = config;
+        _secretKey = secretKey;
+        _accessTokenExpirationMinutes = accessTokenExpirationMinutes;
+    }
+
+    public string GenerateToken(Worker worker)
+    {
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, worker.WorkerId.ToString()),
+            new Claim(ClaimTypes.Name, worker.FirstName),
+            new Claim(ClaimTypes.Name, worker.LastName),
+            new Claim(ClaimTypes.Role, worker.Role.ToString())
+        };
+
+
+        //TODO zaimpplementowac dalsza czesc
+
+        return "";
+    }
+    
+    
+    
+}
